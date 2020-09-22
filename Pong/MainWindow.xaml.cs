949e6afa8ee10142ab990eb31f3294af71ee4e28
TudorDan <tudor.ist@gmail.com>
2020-09-22 10:24:16 +0300

    Add game start and stop

---
+++ Pong/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pong
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                MessageBoxResult response = MessageBox.Show("Are you sure to quit from the best game ever?", "Close confirmation", MessageBoxButton.YesNo);
+                if (response == MessageBoxResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
